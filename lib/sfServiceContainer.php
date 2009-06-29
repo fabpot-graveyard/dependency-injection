01191da8ce7ba2f7e4e0e1400117a808ec0863c0
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-29 06:50:59 +0000

    [dependency_injection] fixed the conversion from a camel-cased string to underscore
    git-svn-id: http://svn.symfony-project.com/components/dependency_injection/trunk@19657 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfServiceContainer.php
+++ lib/sfServiceContainer.php
@@ -355,6 +355,6 @@ class sfServiceContainer implements sfServiceContainerInterface, ArrayAccess, It
 
   static public function underscore($id)
   {
-    return strtolower(preg_replace(array('/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/', '/_/'), array('\\1_\\2', '\\1_\\2', '.'), $id));
+    return strtolower(preg_replace(array('/_/', '/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'), array('.', '\\1_\\2', '\\1_\\2'), $id));
   }
 }
