c9c5e2e849bae71209caa611c0d9209e1d1a2400
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-06-29 14:15:40 +0000

    [dependency_injection] added unit tests (100% coverage)
    git-svn-id: http://svn.symfony-project.com/components/dependency_injection/trunk@19679 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/unit/fixtures/containers/container9.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once dirname(__FILE__).'/../includes/classes.php';
+
+$container = new sfServiceContainerBuilder();
+$container->
+  register('foo', 'FooClass')->
+  setConstructor('getInstance')->
+  setArguments(array('foo', new sfServiceReference('foo.baz'), array('%foo%' => 'foo is %foo%'), true, new sfServiceReference('service_container')))->
+  setFile(realpath(dirname(__FILE__).'/../includes/foo.php'))->
+  setShared(false)->
+  addMethodCall('setBar', array('bar'))->
+  addMethodCall('initialize')->
+  setConfigurator('sc_configure')
+;
+$container->
+  register('bar', 'FooClass')->
+  setArguments(array('foo', new sfServiceReference('foo.baz')))->
+  setShared(true)->
+  setConfigurator(array(new sfServiceReference('foo.baz'), 'configure'))
+;
+$container->
+  register('foo.baz', '%baz_class%')->
+  setConfigurator(array('%baz_class%', 'configureStatic1'))
+;
+$container->register('foo_bar', 'FooClass');
+$container->setParameters(array(
+  'baz_class' => 'BazClass',
+  'foo' => 'bar',
+));
+
+return $container;
---
+++ test/unit/fixtures/includes/classes.php
@@ -0,0 +1,27 @@
+<?php
+
+function sc_configure($instance)
+{
+  $instance->configure();
+}
+
+class BarClass
+{
+}
+
+class BazClass
+{
+  public function configure($instance)
+  {
+    $instance->configure();
+  }
+
+  static public function configureStatic($instance)
+  {
+    $instance->configure();
+  }
+
+  static public function configureStatic1()
+  {
+  }
+}
